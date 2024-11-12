7deb0be27b1dade60800d0acb59166c2a53a4b7e
Miguel Luz <106433901+Migleu@users.noreply.github.com>
2024-11-12 20:22:01 -0300

    1.0

---
+++ Pages/Inicio/index.php
@@ -0,0 +1,43 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../AllStyle.css">
+    <link rel="shortcut icon" href="./Icons/calcLogo.jpeg" type="image/x-icon">
+    <title>Inicio' Minhas finanças</title>
+</head>
+<body>
+    <!-- Navbar -->
+    <div class="Navbar">
+        <div class="logo">
+            <img id="calc" src="../../Componentes/Icons/IconCalc.svg" alt="">
+            <h2>MyFinances</h2>
+        </div>
+        <div class="linkBar">
+            <ul>
+                <li>Inicio</li>
+                <li>Finanças</a></li>
+                <li>Sobre nós</li>
+            </ul>
+        </div>
+        <!-- Centralize dps -->
+        <p>
+            <!-- <img id="calendar" src="../../Componentes/Icons/IconCalendar.svg" alt=""> -->
+            Meses
+        </p>
+    </div>
+    <hr>
+<!-- ///////////////////////// -->
+
+    <div class="container">
+        <h2>Recentes</h2>
+    </div>
+</body>
+</html>
