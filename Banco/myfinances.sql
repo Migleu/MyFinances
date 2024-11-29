712323fcb92a368d0ddffdf1e93f1d748b35923d
Miguel Luz <106433901+Migleu@users.noreply.github.com>
2024-11-29 19:51:00 -0300

    2.1

--- Banco/myfinances.sql
+++ Banco/myfinances.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26/11/2024 às 01:03
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 29-Nov-2024 às 23:50
+-- Versão do servidor: 10.4.21-MariaDB
+-- versão do PHP: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,41 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `categoria`
+-- Estrutura da tabela `categoria`
 --
-CREATE SCHEMA myfinances;
-USE myfinances;
+
 CREATE TABLE `categoria` (
   `id` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `descricao` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `meses`
+-- Estrutura da tabela `meses`
 --
 
 CREATE TABLE `meses` (
   `id` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `ano` int(11) NOT NULL,
-  `resumo_financeiro` decimal(9,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `meses`
---
-
-INSERT INTO `meses` (`id`, `nome`, `ano`, `resumo_financeiro`) VALUES
-(1, '0', 2024, 0.00),
-(2, '1', 2024, 0.00);
+  `resumo_financeiro` decimal(9,2) NOT NULL,
+  `codigoMes` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `transacao`
+-- Estrutura da tabela `transacao`
 --
 
 CREATE TABLE `transacao` (
@@ -69,41 +61,41 @@ CREATE TABLE `transacao` (
   `valor` decimal(9,2) NOT NULL,
   `categoria_movimentacao` int(11) NOT NULL,
   `id_mes` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `transacao`
+-- Extraindo dados da tabela `transacao`
 --
 
 INSERT INTO `transacao` (`id`, `data_transacao`, `tipo`, `descricao`, `valor`, `categoria_movimentacao`, `id_mes`) VALUES
-(6, '2024-11-25', 1, 'Mercado', 50.00, 0, 2),
-(7, '2024-11-25', 0, 'Lazer', 100.00, 2, 2),
-(8, '2024-11-25', 1, 'Uber', 50.00, 1, 0);
+(6, '2024-11-25', 1, 'Mercado', '50.00', 0, 2),
+(7, '2024-11-25', 0, 'Lazer', '100.00', 2, 2),
+(8, '2024-11-25', 1, 'Uber', '50.00', 1, 0);
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `categoria`
+-- Índices para tabela `categoria`
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `meses`
+-- Índices para tabela `meses`
 --
 ALTER TABLE `meses`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `transacao`
+-- Índices para tabela `transacao`
 --
 ALTER TABLE `transacao`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
@@ -116,7 +108,7 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de tabela `meses`
 --
 ALTER TABLE `meses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `transacao`
